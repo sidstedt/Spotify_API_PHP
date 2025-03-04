8161a1239484ddedb38105833515ae1f5ccb9864
SiDiZ <daniel.sidstedt@gmail.com>
2025-03-04 09:26:22 +0100

    Update config.php

--- private/config.php
+++ private/config.php
@@ -1,6 +1,6 @@
 <?php
-$client_ID = 'b185cfae5d4c41f1bf1aac5450924080';
-$client_Secret = 'd61d974c0d5f41d1b85b2f4072a14b6c';
-$redirect_URI = 'http://phpskola.local/Spotify_API/callback.php';
+$client_ID = 'your-client-id';
+$client_Secret = 'your-client-secret-pass';
+$redirect_URI = 'your-address/Spotify_API/callback.php';
 $get_token_url = 'https://accounts.spotify.com/api/token';
 ?>
